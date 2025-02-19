c8674dbf136950f03e618bfcd7a0d18bb2433b6a
Prince <shashwatk806@gmail.com>
2025-02-19 17:25:43 +0530

    todays push

--- Lecture-43/Express/index.js
+++ Lecture-43/Express/index.js
@@ -11,7 +11,23 @@ const express = require('express'); // function// sklfhfhffbufhjhhvnlnlasqw
 
 const app = express(); // return something -> main application object.
 // this app is our entire instance for application.
-console.log(app);
+// console.log("samarth bhai backend");
 
 //----------------------------------
+// middleware function
+// app.use(function(req, res){
+//     console.log('you made a request');
+// })
+// runs on every incoming request
+app.use((req, res)=>{
+    console.log('you made a request');
+    // console.log(req);
+    // res.send('hi, server gave the response.')
+    res.send(<h1>hi server gave the response.</h1>)
+})
+
+app.listen(8080, function(){    // 2 arguements port no , cb function
+    console.log("server connected at port 8080 mst hai")
+})
+
 
