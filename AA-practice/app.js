ca71f2bbeed3a35e81cbe88b0dd0efa171bcab0b
Prince <shashwatk806@gmail.com>
2025-04-14 15:28:26 +0530

    today's code

--- AA-practice/app.js
+++ AA-practice/app.js
@@ -1,30 +1,4 @@
-
-
-fetch('https://api.tvmaze.com/search/shows?q=girls')
-.then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data);
-})
-.catch(function(error){
-    console.log(error);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"use strict";
+b = "ankur";
+b = "shashwat";
+console.log(b);
