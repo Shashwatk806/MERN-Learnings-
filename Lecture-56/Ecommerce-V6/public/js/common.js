a9b0bbf59e21d2b216ea19a9adf4baa784985710
shashwatk806 <shashwatk806@gmail.com>
2025-04-17 09:36:18 +0530

    Ecommerce-V6

--- Lecture-56/Ecommerce-V6/public/js/common.js
+++ Lecture-56/Ecommerce-V6/public/js/common.js
@@ -2,11 +2,13 @@
 let allLikeBtn = document.querySelectorAll('.like-btn');
 
 function likeButton(){
-    console.log('like the button')
+    // console.log('like the button')
 }
 
 for(let btn of allLikeBtn){
     btn.addEventListener('click',()=>{
+        console.log(btn.getAttribute('product-id'));
+        
         likeButton();
     })
 }
