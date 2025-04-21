48be2f331dd9d0ba75440e54167f54729c8665fa
shashwatk806 <shashwatk806@gmail.com>
2025-04-21 17:14:14 +0530

    React

---
+++ Lecture-58/React/app.js
@@ -0,0 +1,42 @@
+
+//DOM -> operatonally very heavy
+// let rootEl = document.getElementById('root');
+// let h1 =document.createElement('h1');
+
+// h1.innerHTML = "Hello h1 from DOM";
+// rootEl.append(h1);
+
+//-------------------------------------------------
+//React -> 
+// let rootEl = document.getElementById('root');
+// // let h1 = React.createElement('h1', null, "Hello h1 from React");
+// let h1 = React.createElement(
+//     'h1', 
+//     {id:"sam", className:"vohra"}, // we cant directly class bcs it is a reserved keyword by js
+//      "Hello h1 from React 18"
+//     );
+
+// // to display it
+// // ReactDOM.render(h1,rootEl);
+// let root = ReactDOM.createRoot(rootEl);
+// root.render(h1);
+// root.render(h1,h2); //wrong it accepts only one argument
+
+//---------------------------------------------------
+
+let rootEl = document.getElementById('root');
+let h1 = React.createElement(
+    'h1', 
+    {id:"sam", className:"vohra"}, // we cant directly class bcs it is a reserved keyword by js
+     "Hello h1 from React 18"
+    );
+ let h2 = React.createElement(
+        'h2', 
+        null,
+         "Hello h2 from React 18"
+        );
+// to display it
+// ReactDOM.render(h1,rootEl);
+let root = ReactDOM.createRoot(rootEl);
+root.render([h1,h2]);
+
