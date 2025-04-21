d7147874884790b6e12d72b776f81388872e78ea
shashwatk806 <shashwatk806@gmail.com>
2025-04-21 17:14:59 +0530

    React

--- Lecture-58/React/app.js
+++ Lecture-58/React/app.js
@@ -24,19 +24,27 @@
 
 //---------------------------------------------------
 
-let rootEl = document.getElementById('root');
-let h1 = React.createElement(
-    'h1', 
-    {id:"sam", className:"vohra"}, // we cant directly class bcs it is a reserved keyword by js
-     "Hello h1 from React 18"
-    );
- let h2 = React.createElement(
-        'h2', 
-        null,
-         "Hello h2 from React 18"
-        );
-// to display it
-// ReactDOM.render(h1,rootEl);
-let root = ReactDOM.createRoot(rootEl);
-root.render([h1,h2]);
+// let rootEl = document.getElementById('root');
+// let h1 = React.createElement(
+//     'h1', 
+//     {id:"sam", className:"vohra"}, // we cant directly class bcs it is a reserved keyword by js
+//      "Hello h1 from React 18"
+//     );
+//  let h2 = React.createElement(
+//         'h2', 
+//         null,
+//          "Hello h2 from React 18"
+//         );
+// // to display it
+// // ReactDOM.render(h1,rootEl);
+// let root = ReactDOM.createRoot(rootEl);
+// root.render([h1,h2]);
+
+//-----------------------------------------------
+
+``
+
+
+
+
 
