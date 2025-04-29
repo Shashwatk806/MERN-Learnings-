1213e11f7ea8f8986c2d3a20449f08dba0360b2d
shashwatk806 <shashwatk806@gmail.com>
2025-04-29 18:23:49 +0530

    quote-app

--- Lecture-63/QuotesApp/backend/app.js
+++ Lecture-63/QuotesApp/backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const seedDB = require('./seed');
 const quoteRoutes = require('./apis/quotesroutes');
@@ -11,12 +12,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/quotes')
     console.log("Error in DB",err);
   });
 
+  app.use(cors({
+    origin:["http://localhost:5173"]
+  }))
+
+  app.use(cors());
   app.use(express.json()) // for parsing application/json
   app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
   
-// app.get('/', (req,res)=>{
-//     res.status(200).json({msg:"hello from root"})
-// })
+app.get('/', (req,res)=>{
+    res.status(200).json({msg:"hello from root"})
+})
 
 app.use(quoteRoutes);
 
