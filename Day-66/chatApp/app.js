dc2c3f099f081037da42296ac9c8e65fa4b5a641
shashwatk806 <shashwatk806@gmail.com>
2025-05-01 18:56:12 +0530

    extra

---
+++ Day-66/chatApp/app.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+const http = require('http');
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log('server connected at port 3000')
+})
+
