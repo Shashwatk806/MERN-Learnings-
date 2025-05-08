ed3bfa1b966d9d86217e334e76214b70569179e4
shashwatk806 <shashwatk806@gmail.com>
2025-05-08 18:06:32 +0530

    code

--- AA-practice/app.js
+++ AA-practice/app.js
@@ -1,2 +1,18 @@
 
-const naam = "Prince";
+
+function add(a){
+    return a*5;
+}
+
+add.power=2;
+console.log(add(3));
+
+
+
+let a ={
+    name: "ankur",
+    age: 18
+}
+
+console.log(a['name']);
+console.log(a['age']);
