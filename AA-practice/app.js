3c9a2f2e272c8c4ee9259af707f314c5cb44aca8
shashwatk806 <shashwatk806@gmail.com>
2025-05-09 20:10:50 +0530

    hd

--- AA-practice/app.js
+++ AA-practice/app.js
@@ -1,18 +1,4 @@
 
 
-function add(a){
-    return a*5;
-}
-
-add.power=2;
-console.log(add(3));
 
 
-
-let a ={
-    name: "ankur",
-    age: 18
-}
-
-console.log(a['name']);
-console.log(a['age']);
