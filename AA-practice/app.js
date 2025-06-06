1bb1ba17636db560826a393317da20715516d31b
shashwatk806 <shashwatk806@gmail.com>
2025-06-06 12:16:04 +0530

    asynchrous nature of js

--- AA-practice/app.js
+++ AA-practice/app.js
@@ -1,14 +1,55 @@
 
-console.log('start');
-setTimeout(function(){
-    console.log("set timeout line 4");
-},4000)
-setTimeout(function(){
-    console.log("set timeout line");
-},0)
-console.log("end");
 
+let step1 = function(){
+    console.log('Selecting image')
+    return new Promise(function(resolve,reject){
+        setTimeout(() => {
+            resolve('photo.jpg');
+        },4000);
+    })
+}
 
+let step2 = function(image){
+    console.log(`applying filter to the ${image}`);
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+        resolve('filter applied')
+        },3000)
+    })
+}
 
+let step3 = function(filtered){
+    console.log(`applying caption to the ${filtered}`);
+    return new Promise(function(resolve,reject){
+        setTimeout(() => {
+            resolve('caption done')
+        },3000);
+    })
+}
+
+let step4 = function(captioned){
+     console.log(`uploading the ${captioned} please wait...`);
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            resolve('uploaded successfully,plz like and share.')
+        },5000)
+    })
+}
+
+step1().then(function(image){
+    console.log('image selected');
+    return step2(image)
+}).then(function(filtered){
+    console.log('filter applied');
+    return step3(filtered)
+}).then(function(captioned){
+    console.log('caption applied');
+    return step4(captioned)
+}).then(function(post){
+    console.log(post)
+}).catch(function(err){
+    console.log(err);
+    
+})
 
 
---
+++ AA-practice/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+step1.then(function(image){
+//     console.log('image selected');
+//     return step2(image)
+// })
