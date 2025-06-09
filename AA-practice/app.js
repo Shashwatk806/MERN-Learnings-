e702c91c04ceeb625a8637b11574a93ccaf074e2
shashwatk806 <shashwatk806@gmail.com>
2025-06-09 17:33:30 +0530

    api fetch

--- AA-practice/app.js
+++ AA-practice/app.js
@@ -1,16 +1,8 @@
 
-let inp = document.querySelector('input');
-let btn = document.querySelector('button');
-let ul = document.querySelector('ul');
-
-btn.addEventListener('click',function(){
-    let data = inp.value;
-    // console.log(data);
-    let li = document.createElement('li');
-    li.innerHTML= data;
-    ul.append(li);
-    inp.value = "";
-    li.addEventListener('click',function(){
-        li.remove();
-    })
-})
+// XHR request 
+let xhr = new XMLHttpRequest();
+xhr.open('get','https://api.tvmaze.com/search/shows?q=girls')
+xhr.onload = function(){
+ 
+}
+xhr.send();
