6da0ac9d95394b171262b13988cc3a299696bb98
shashwatk806 <shashwatk806@gmail.com>
2025-06-09 16:27:35 +0530

    todays code

--- AA-practice/app.js
+++ AA-practice/app.js
@@ -1,56 +1,16 @@
 
+let inp = document.querySelector('input');
+let btn = document.querySelector('button');
+let ul = document.querySelector('ul');
 
-let step1 = function(){
-    console.log('Selecting image')
-    return new Promise(function(resolve,reject){
-        setTimeout(() => {
-            resolve('photo.jpg');
-        },4000);
+btn.addEventListener('click',function(){
+    let data = inp.value;
+    // console.log(data);
+    let li = document.createElement('li');
+    li.innerHTML= data;
+    ul.append(li);
+    inp.value = "";
+    li.addEventListener('click',function(){
+        li.remove();
     })
-}
-
-let step2 = function(image){
-    console.log(`applying filter to the ${image}`);
-    return new Promise(function(resolve,reject){
-        setTimeout(function(){
-        resolve('filter applied')
-        },3000)
-    })
-}
-
-let step3 = function(filtered){
-    console.log(`applying caption to the ${filtered}`);
-    return new Promise(function(resolve,reject){
-        setTimeout(() => {
-            resolve('caption done')
-        },3000);
-    })
-}
-
-let step4 = function(captioned){
-     console.log(`uploading the ${captioned} please wait...`);
-    return new Promise(function(resolve,reject){
-        setTimeout(function(){
-            resolve('uploaded successfully,plz like and share.')
-        },5000)
-    })
-}
-
-step1().then(function(image){
-    console.log('image selected');
-    return step2(image)
-}).then(function(filtered){
-    console.log('filter applied');
-    return step3(filtered)
-}).then(function(captioned){
-    console.log('caption applied');
-    return step4(captioned)
-}).then(function(post){
-    console.log(post)
-}).catch(function(err){
-    console.log(err);
-    
 })
-
-
-
--- AA-practice/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-step1.then(function(image){
-//     console.log('image selected');
-//     return step2(image)
-// })
